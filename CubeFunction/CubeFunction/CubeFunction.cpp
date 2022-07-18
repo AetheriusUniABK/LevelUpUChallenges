15f2d1cc598f2593e2c649a0cd03714b5377aa8a
AetheriusUniABK <marcraymond.serrano@activision.com>
2022-07-18 15:23:59 -0700

    submit SumAverage and CubeFunction

---
+++ CubeFunction/CubeFunction/CubeFunction.cpp
@@ -0,0 +1,31 @@
+// CubeFunction.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int cube(int num);
+
+int main()
+{
+    int inputNum;
+    cout << "Enter a number you want to be cubed: ";
+    cin >> inputNum;
+    cout << "Your number cubed is: " << cube(inputNum) << endl;
+}
+
+int cube(int num)
+{
+    return num * num * num;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
